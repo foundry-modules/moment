d1d7555ba65e6d0e8c70568537c9af0478ac62b5
Jensen Tonne <jensen.tonne@gmail.com>
2013-08-20 17:26:44 +0800

    Export moment to $.moment.

--- moment.js
+++ moment.js
@@ -1715,23 +1715,6 @@
     /************************************
         Exposing Moment
     ************************************/
+    $.moment = moment;
 
-
-    // CommonJS module is defined
-    if (hasModule) {
-        module.exports = moment;
-    }
-    /*global ender:false */
-    if (typeof ender === 'undefined') {
-        // here, `this` means `window` in the browser, or `global` on the server
-        // add `moment` as a global object via a string identifier,
-        // for Closure Compiler "advanced" mode
-        this['moment'] = moment;
-    }
-    /*global define:false */
-    if (typeof define === "function" && define.amd) {
-        define("moment", [], function () {
-            return moment;
-        });
-    }
 }).call(this);
